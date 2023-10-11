1d84aa298cfbb24f115cd717c0c0fafceafd9730
JudyCodberg <128333114+JudyCodberg@users.noreply.github.com>
2023-10-11 12:08:40 +0900

    fix : update wed address

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,8 @@ const BASE_URL = "http://localhost:8000";
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    // "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": "http://judy-board.s3-website.ap-northeast-2.amazonaws.com/",
     "Access-Control-Allow-Credentials": "true",
   },
 });
