c0da792419771fddf5e7cdaf23962d002ef9d694
JudyCodberg <128333114+JudyCodberg@users.noreply.github.com>
2023-10-11 14:16:43 +0900

    update url

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     // "Access-Control-Allow-Origin": "http://localhost:3000",
-    // "Access-Control-Allow-Origin": "http://judy-board.s3-website.ap-northeast-2.amazonaws.com/",
+    "Access-Control-Allow-Origin": "http://52.78.0.169:3000/",
     "Access-Control-Allow-Credentials": "true",
   },
 });
